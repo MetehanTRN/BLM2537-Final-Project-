61989eb693edd4ff697532dcf1d8eddbec944118
Metehan Turan <95168071+MetehanTRN@users.noreply.github.com>
2024-01-09 22:00:09 +0300

    Add files via upload

---
+++ BLM2537/fikstur.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var table = document.getElementById("myTable");
+    var text = document.querySelector(".animated-text");
+
+    // Animasyonlu tablo için stil eklenmesi
+    table.classList.add("animation-show");
+
+    // Animasyonlu yazı için stil eklenmesi
+    text.classList.add("text-show");
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var table = document.getElementById("myTables");
+    var text = document.querySelector(".animated-text");
+
+    // Animasyonlu tablo için stil eklenmesi
+    table.classList.add("animation-show");
+
+    // Animasyonlu yazı için stil eklenmesi
+    text.classList.add("text-show");
+  });
